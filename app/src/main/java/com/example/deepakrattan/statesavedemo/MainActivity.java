69eb6f491ab95428fce1bcc75b6ff43dc2cfe5c2
Deepak Rattan <mca.deepakrattan@gmail.com>
2017-03-15 22:56:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/deepakrattan/statesavedemo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.deepakrattan.statesavedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "State_Save";
+    private EditText edtName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        edtName = (EditText) findViewById(R.id.edtName);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    //Saving the state of activity when the orientation changes
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
+        String name = edtName.getText().toString();
+        outState.putString("name_key", name);
+    }
+
+    //Restore the state of the activity when the orientation changes
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: ");
+        String name = savedInstanceState.getString("name_key");
+        edtName.setText(name);
+    }
+}
